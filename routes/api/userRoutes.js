6ce2ed5c10a62277465028d8d12c646dc5ed357f
shakib42160 <shahadatsakib49@gmail.com>
2025-04-27 05:49:27 -0400

    Fixed bcrypt issue for Render deployment

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../../models');  // <<< CORRECTED
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Register Route
 router.get('/register', (req, res) => {
